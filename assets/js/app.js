63ebdbd5a12b00db668c1441e3573740984163fb
LukeB <luke.burnett@gsa.gov>
2022-11-30 16:02:01 -0500

    Edit the Latest Updates section increase the smoothness transition between cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -199,61 +199,79 @@ function initButtons() {
 
 //
 var timeOut = 5000;
-var slideIndex = 0;
+var slideIndex = 1;//The next latest update instead of the first one
 var autoOn = true;
 
 function autoSlides() {
-    timeOut = timeOut - 20;
-    console.log("autoSlides() ran");
-    if (autoOn == true && timeOut < 0) {
-      console.log("showSLides() ran");
-        showSlides();
-    }
-    setTimeout(autoSlides, 20);
+  timeOut = timeOut - 20;
+
+  if (autoOn == true && timeOut < 0) {
+      showSlides();
+  }
+  setTimeout(autoSlides, 20);
 }
 
 function prevSlide() {
 
-    timeOut = 2000;
+  timeOut = 5000;
 
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex--;
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex--;
 
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    if (slideIndex == 0) {
-        slideIndex = 3
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  if (slideIndex > slides.length) {
+      slideIndex = 1
+  }
+  if (slideIndex == 0) {
+      slideIndex = 3
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 function showSlides() {
 
-    timeOut = 5000;
+  timeOut = 5000;
 
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex++;
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
 
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+  if (slideIndex > slides.length) {
+      slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+// function showSlides() {
+
+//   timeOut = 5000;
+
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slideIndex++;
+
+//   if (slideIndex > slides.length) {
+//       slideIndex = 1
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
 
 autoSlides();
 
