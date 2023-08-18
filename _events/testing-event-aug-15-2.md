a87270c787b5e28296246b5da91af3fddf8e337e
LukeB <luke.burnett@gsa.gov>
2023-08-18 15:33:22 -0400

    Edited Calendar tab on the Event page.
    - Edited the calendar code and able to retrieved all event files which are markdown and make it compatible with the pre-existing calendar algorithm.
    
    - Edited the calendar design according to the redesign mockup. (Desktop version)

---
+++ _events/testing-event-aug-15-2.md
@@ -0,0 +1,11 @@
+---
+organizer: TEST
+url-link: www.youtube.com/
+title: Testing Event Aug 15 (2)
+description: Testing Event Aug 15 Description (2)
+start-time: 2023-08-15T11:30:00.000-00:00
+end-time: 2023-08-15T12:30:00.000-00:00
+event-type: Online
+gov-only: true
+is-external: true
+---
--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,7 @@ if(document.getElementById('page-directory') != null) //Other page beside homepa
 //Run Events page
 let nav = 0;
 let clicked = null;
-let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+// let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 const eventList = [], pastEventList = [], futureEventList = [];
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
@@ -635,6 +635,7 @@ function retriveEventsData()
       "date":startTime,
       "day":weekdaysShort[startTime.getDay()],
       "dateNumber":startTime.getDate(),
+      "month":startTime.getMonth(),
       "fromTo":eventTime,
       "eventType":ev.getAttribute("data-event-type"),
       "govOnly":ev.getAttribute("data-gov-only"),
@@ -651,19 +652,14 @@ function retriveEventsData()
      }
 
   }
-
-  console.log("All Events:", eventList);
-  console.log("Past Events:", pastEventList);
-  console.log("Future Events:", futureEventList);
-
 }
 
 //This function extract the time from the date, and assign AM or PM depend on the time.
 function getDateTime(date) 
 {
-  const hours = date.getUTCHours();
+  const hours = date.getUTCHours(); //Eventhough it UTC but we will treat it as EST.
   const minutes = date.getUTCMinutes();
-
+ 
   const ampm = hours >= 12 ? "PM" : "AM";
 
   const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
@@ -675,53 +671,64 @@ function getDateTime(date)
 
 /** Event page calendar **/
 
-function openModal(date) {
-  const eventForDay = [];
-
-  for(var i = 0; i < events.length; i++)
-  { 
-    if(events[i].date === date)
-    {
-      eventForDay.push(events[i]);
-    }
-  }
-  //event already exist
+function openModal(eventForDay, dayString) {
   if(eventForDay.length > 0)
   {
-    var eventText = ``;
+    // var eventText =
+    // `
+    // <h3 id="calendar-date-display">${dayString}</h3>
+    // <div class="animate fadeInDown">
+    // `;
+    var eventText =
+    `
+    <h3 id="calendar-date-display">${dayString}</h3>
+    <div>
+    `;
     for(var i = 0; i < eventForDay.length; i++)
     { 
-      var audience = "";
-      if(i == 0)
-        eventText = 
-        `
-        <div class="animate fadeInDown">
-        <h2 id="eventsSummary">Events Summary:</h2>
-        <h3>${date}</h3>
-        `;
-      if(eventForDay[i].color === "GREY")
-        audience = " This is a partner event.";
-      else if(eventForDay[i].color === "RED")
-        audience = " This is open to all.";
-      else if(eventForDay[i].color === "BLUE")
-        audience = " This is open to .gov/.mil only.";
-
+      let colorLine = "#d36c6c;"
+      let inEx = "ITVMO Event"
+      //If the event is external the line under the title change to blue
+      if(eventForDay[i].isExternal == "true")
+      {
+        
+        colorLine = "#1b2b85";
+        inEx = "Non-ITVMO Event"
+      }
       eventText += 
         `
-        <p><a href="${eventForDay[i].link}" target="_blank" rel="noreferrer noopener"><b>${eventForDay[i].title} </b></a></p>
-        <p id="eventDescription">${eventForDay[i].description}<b>${audience}</b></p>
+        <div class="event">
+          <h3 class="event-org">${eventForDay[i].organizer}</h3>
+          <h2 class="event-title">${eventForDay[i].title}</h2>
+          <div class="event-color" style="background-color:${colorLine}"></div>
+          <p class="event-description">${eventForDay[i].description}</p>
+          <div class="event-info-link">
+            <div class="event-info">
+              <p><img src="${baseUrl}/assets/images/icons/clock-icon-grey.svg">${eventForDay[i].fromTo}</p>
+              <p><img src="${baseUrl}/assets/images/icons/location-icon-grey.svg">${eventForDay[i].eventType}</p>
+              <p><img src="${baseUrl}/assets/images/icons/compass-icon-grey.svg">${inEx}</p>
+            </div>
+            <a aria-label="Event link of Organization:${eventForDay[i].organizer}, Title:${eventForDay[i].title}" class="event-link" href="${eventForDay[i].link}" target="_blank" rel="noreferrer noopener"><b><img src="${baseUrl}/assets/images/icons/external-white.svg">View Event</b></a>
+          </div>
+        </div>
         `;
     }
     eventText += `</div>`
-    document.getElementById('eventText').innerHTML = eventText;
+    document.getElementById('current-day-events').innerHTML = eventText;
   }
   else
   {
-    document.getElementById('eventText').innerHTML = 
+    // document.getElementById('current-day-events').innerHTML = 
+    // `
+    // <h3 id="calendar-date-display">${dayString}</h3>
+    // <div class="animate fadeInDown">
+    // <p><b>No events planned today.</b></p>
+    // </div>
+    // `;
+    document.getElementById('current-day-events').innerHTML = 
     `
-    <div class="animate fadeInDown">
-    <h2 id="eventsSummary">Events Summary:</h2>
-    <h3>${date}</h3>
+    <h3 id="calendar-date-display">${dayString}</h3>
+    <div>
     <p><b>No events planned today.</b></p>
     </div>
     `;
@@ -755,20 +762,20 @@ async function runCalendar(d,m,y) {
     monthNum = dt.getMonth();
     year = dt.getFullYear();
   }
-  //Since only data that avaliable is currently only 2022 and 2023
-  if((year >= 2022)&&(year <= 2023))
+      //This function get the specific month from the calendar.
+  const getCurrMonthEvents = () => 
   {
-      //retrive the month information
-      const res = await fetch(`../assets/events/${year}/${month[monthNum]}.json`);
-      var thisMonthEvents = await res.json(); //data in this case is array list of items
-      thisMonthEvents = thisMonthEvents.events;
-     //Check whether the json file already store in the local storage already or not.
-      for(let i = 0; i<thisMonthEvents.length; i++)
+    const currMonthEvents = [];
+    for(ev of eventList)
+    {
+      if((monthNum == ev.date.getMonth()) && (year == ev.date.getFullYear()))
       {
-        if(events.find(e => (e.title === thisMonthEvents[i].title) && (e.date === thisMonthEvents[i].date)) == null)
-          saveEvent(thisMonthEvents[i]);
+        currMonthEvents.push(ev);
       }
+    }
+    return currMonthEvents;
   }
+      var thisMonthEvents = getCurrMonthEvents();
   //Generate calendar
   const firstDayOfMonth = new Date(year, monthNum, 1);
   const daysInMonth = new Date(year, monthNum + 1, 0).getDate();
@@ -779,84 +786,118 @@ async function runCalendar(d,m,y) {
     day: 'numeric',
   });
   const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+  const monthString = dt.toLocaleDateString('en-us', { month: 'long' })
   document.getElementById('monthDisplay').innerText = 
-    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+    `${monthString} ${year}`;
 
   calendar.innerHTML = '';
 
-  for(let i = 1; i <= paddingDays + daysInMonth; i++) {
+  for(let i = 1; i <= paddingDays + daysInMonth; i++) 
+  {
+    let itvmoEventCount = 0;
     const daySquare = document.createElement('div');
     daySquare.classList.add('day'); 
-    const dayString = `${monthNum + 1}/${i - paddingDays}/${year}`;
+    const dayString = `${monthString} ${i - paddingDays}, ${year}`;
 
-    if (i > paddingDays) {
-      daySquare.innerText = i - paddingDays;
+    //Round the corner of the specific date to make the calendar corner look round.
+    if(i == 1)
+    {
+      daySquare.classList.add('day-corner-top-left'); 
+    }
+    else if(i == 7)
+    {
+      daySquare.classList.add('day-corner-top-right'); 
+    }
+    else if(((i == 29)&&((daysInMonth + paddingDays) <= 35)) || (i == 36))
+    {
+      daySquare.classList.add('day-corner-bottom-left'); 
+    }
+    else if((i == (paddingDays + daysInMonth))&&(((paddingDays + daysInMonth)%7)==0))
+    {
+      daySquare.classList.add('day-corner-bottom-right'); 
+    }
+
+    if (i > paddingDays) 
+    {
+      daySquare.innerHTML = `<p>${i - paddingDays}</p>`;
       const eventForDay = [];
-      //These two variable use to determine the color of the squre on specific date of the calendar.
-      var itvmoEvent = false; 
-      var otherEvent = false;
-      for(var a = 0; a < events.length; a++)
+      for(var a = 0; a < thisMonthEvents.length; a++)
       { 
-        if(events[a].date === dayString)
+        if((thisMonthEvents[a].date.getMonth() == monthNum)&&(thisMonthEvents[a].date.getDate() == (i - paddingDays))&&(thisMonthEvents[a].date.getFullYear()==year))
         {
-          eventForDay.push(events[a]);
-          //!!Change this later use other field to determine
-          if(otherEvent == false && events[a].type == "OTHER")
-            otherEvent = true;
-          //!!Change this later
-          if(itvmoEvent == false && events[a].type == "ITVMO")
-            itvmoEvent = true;
+          eventForDay.push(thisMonthEvents[a]);
+          if(thisMonthEvents[a].isExternal == "false")
+          {
+            itvmoEventCount++;
+          }
         }
       }
+      let nonItvmoCount = eventForDay.length - itvmoEventCount;
       daySquare.title = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${i - paddingDays} event count of ${eventForDay.length}`;
       //Display the Events summary for the current day if there is one
       if ((i - paddingDays == day && nav === 0)) {
         daySquare.id = 'currentDay';
+        daySquare.classList.add('active-day');
         daySquare.tabIndex = 0;
-        openModal(dayString);
+        openModal(eventForDay, dayString);
       }
-
-      if (eventForDay.length > 0) {
+      if (eventForDay.length > 0) 
+      {
           //Only let user traverse on the calendar with tab on only the day that have event(s).
           daySquare.tabIndex = 0;
-          const eventDiv = document.createElement('div');
-          eventDiv.classList.add('event');
-          eventDiv.innerText += eventForDay.length + " EVENTS";
-          daySquare.appendChild(eventDiv);
-
-          const eventDivM = document.createElement('div');
-          eventDivM.classList.add('eventMobile');
-          eventDivM.innerText += eventForDay.length;
-
-          //If ITVMO events only on specific date
-          if(otherEvent == false && itvmoEvent == true)
-            eventDivM.style.cssText += 'background-color: #d36c6c;';
-          //If Other events only on specific date
-          else if(otherEvent == true && itvmoEvent == false)
-            eventDivM.style.cssText += 'background-color: #1b2b85;';
-          //If both ITVMO and other events on specific date
-          else if(otherEvent == true && itvmoEvent == true)
-            eventDivM.style.cssText += 'background: linear-gradient(135deg, #d36c6c 50%, #1b2b85 50%); ';
+          const eventCountSection = document.createElement('div');
+          eventCountSection.classList.add('event-count-section');
           
-          itvmoEvent = false;
-          otherEvent = false;
-          daySquare.appendChild(eventDivM);
-
+          if(itvmoEventCount > 0)
+          {
+            const eventDivM = document.createElement('div');
+            eventDivM.classList.add('event-count');
+            eventDivM.innerText += itvmoEventCount;
+            eventDivM.style.cssText += 'background-color: #d36c6c;';
+            eventCountSection.appendChild(eventDivM);
+          }
+          if(nonItvmoCount > 0)
+          {
+            const eventDivM = document.createElement('div');
+            eventDivM.classList.add('event-count');
+            eventDivM.innerText += nonItvmoCount;
+            eventDivM.style.cssText += 'background-color: #1b2b85;';
+            eventCountSection.appendChild(eventDivM);
+          }
+          daySquare.appendChild(eventCountSection);
       }
       //add event listener on each day for clicking on the day action
-      daySquare.addEventListener('click', () => openModal(dayString));
+      daySquare.addEventListener('click', () => 
+      {
+        setActiveDay(daySquare);
+        openModal(eventForDay, dayString)
+      });
       daySquare.onkeydown = 
       function(key) 
       {
         if((key.keyCode === 32)||(key.keyCode === 13))
-          openModal(dayString);
+        {
+          setActiveDay(daySquare);
+          openModal(eventForDay, dayString);
+        }
       }
     } 
-    else {
+    else 
+    {
       daySquare.classList.add('padding');
     }
     calendar.appendChild(daySquare);    
   }
+}
+//This function change the background of the day when user click on the day.
+function setActiveDay(daySquare)
+{
+  const activeDay = document.getElementsByClassName('active-day');
+  if(activeDay.length > 0)
+    {
+      activeDay[0].classList.remove('active-day');
+    }
+  daySquare.classList.add('active-day');
 }
 //Save the event 
 function saveEvent(currEvent) {
